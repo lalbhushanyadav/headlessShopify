f5d855d8112b9deb2e7c7468a220bdab34890b40
Lal Bhushan Yadav <lal.yadav@codeclouds.in>
2025-04-21 20:06:06 +0530

    My Account added

---
+++ src/apps/frontend/components/AccountOrders.jsx
@@ -0,0 +1,10 @@
+const AccountOrders = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Your Orders</h1>
+      <p>Coming soon... show list of orders fetched from Shopify.</p>
+    </div>
+  );
+};
+
+export default AccountOrders;
